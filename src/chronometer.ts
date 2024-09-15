da483f6bc27aa6b38aabc5d15236f8e52d0188ea
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-09-15 20:11:57 +0100

    feat: Implement the start method for the Chronometer.

--- src/chronometer.ts
+++ src/chronometer.ts
@@ -6,4 +6,22 @@ export class Chronometer {
     constructor(timeInSeconds: number) {
         this.remainingTime = timeInSeconds;
     }
+
+
+    start(): void {
+        if (this.intervalId) return;
+
+        console.log(`Starting countdown from ${this.remainingTime} seconds...`);
+
+        this.intervalId = setInterval(() => {
+            if (this.remainingTime <= 0) {
+                this.stop();
+                console.log("Time's up!");
+            } 
+            else {
+                this.remainingTime -= 1;
+                console.log(`Remaining time: ${this.remainingTime} seconds`);
+            }
+        }, 1000);
+    }    
 }
