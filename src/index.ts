d6651a297d7c4a1189fbeca683b21f31198de0d5
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-09-15 20:14:09 +0100

    feat: feat: Adding an I/O interface.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Chronometer } from './chronometer';
+import * as readline from 'readline';
+
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
